9f4caba953866c08167e14ef9fc47776056c293e
PetarKap <kapitanovicpetar@gmail.com>
2026-02-16 19:07:39 +0100

    dodao sam klase Admin  i Životinja

---
+++ azil/Zivotinja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace azil
+{
+    public class Zivotinja
+    {
+         string ime { get; set; }
+         string pasmina { get; set; }
+        string vrsta { get; set; }
+        int godine { get; set; }
+        RadioButton Spol { get; set; }
+        DateTime DatumDolaska { get; set; }
+        CheckBox Cijepljen { get; set; }
+        CheckBox Kastriran { get; set; }
+        List<string> Napomena { get; set; }
+
+
+
+    }
+}
