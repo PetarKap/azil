92aeaeeee3b69e39d4acd20c6f4b79b91dfda869
PetarKap <kapitanovicpetar@gmail.com>
2026-03-12 13:10:30 +0100

    radio na admin formi i ostalim funckijama pozivanja

--- azil/Admin.cs
+++ azil/Admin.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace azil
 {
-    public  class Admin
+    public class Admin
     {
         private static string filePath = "zivotinje.txt";
         public static void SpremiZivotinju(string zapis)
@@ -15,8 +15,52 @@ namespace azil
             using (StreamWriter sw = new StreamWriter(filePath, true))
             {
                 sw.WriteLine(zapis);
-            }   
+            }
         }
-        
+        public static List<string> UcitajZivotinje()
+        {
+            List<Zivotinja> zivotinje = new List<Zivotinja>();
+            if (File.Exists(filePath))
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] podaci = line.Split(',');  
+                        Zivotinja k = new Zivotinja();
+                       k.ime = podaci[0];
+
+                        zivotinje.Add(k);
+                        
+                    }
+                }
+            }
+            return zivotinje;
+        }
+        public static List<string> PretraziZivotinje(string kriterij)
+        {
+            List<string> rezultat = new List<string>();
+            List<string> zivotinje = UcitajZivotinje();
+            foreach (string zivotinja in zivotinje)
+            {
+                if (zivotinja.Contains(kriterij))
+                {
+                    rezultat.Add(zivotinja);
+                }
+            }
+            return rezultat;
+
+        }
+    } 
+    public static List<string> Pretvori()
+        {
+            List<string> rezultat = new List<string>();
+            List<ZI>
+
+
+        } 
+    
     }
+
 }
